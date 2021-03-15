f6b84901414b3e89cd4e866b0c38a534b23559d3
twesleyb <twesleyb10@gmail.com>
2021-03-15 10:55:27 -0400

    try documenting data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' hi - a simple R object
+#' 
+#' Save R objects with save and access their documentation with the help
+#' commend. For example, help("ozone",package="plyr").
+#' 
+#' @docType data
+#' @name hi
+#' @usage hi
+#' @format an R \cite{"character"}
+#' @keywords datasets
+#' @references \url{https://github.com/hadley/plyr/blob/master/R/data.r}
